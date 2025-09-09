1190e491df210f934151386edaeef8319291cf18
Kang Min <min.kang@villanema.com>
2025-09-09 23:32:04 +0900

    Add log message

--- security/src/main/java/edu/cnu/swacademy/security/auth/AuthService.java
+++ security/src/main/java/edu/cnu/swacademy/security/auth/AuthService.java
@@ -1,5 +1,6 @@
 package edu.cnu.swacademy.security.auth;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ import edu.cnu.swacademy.security.user.User;
 import edu.cnu.swacademy.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class AuthService {
@@ -28,6 +30,7 @@ public class AuthService {
 
     User user = userRepository.findByEmail(request.userEmail())
         .orElseThrow(() -> new SecurityException(ErrorCode.USER_NOT_FOUND));
+    log.info("Got user id (={})", user.getId());
 
     String hashedPassword = HashUtil.sha512(request.userPassword());
     if (!user.getPassword().equals(hashedPassword)) {
@@ -58,6 +61,7 @@ public class AuthService {
 
     Authentication authentication = authenticationRepository.findByUserIdAndRefreshToken(userId, request.refreshToken())
         .orElseThrow(() -> new SecurityException(ErrorCode.REFRESH_TOKEN_NOT_FOUND));
+    log.info("Got authentication id (={})", authentication.getId());
 
     TokenInfo newTokenInfo = jwtUtil.generateAccessAndRefreshToken(userId);
 
