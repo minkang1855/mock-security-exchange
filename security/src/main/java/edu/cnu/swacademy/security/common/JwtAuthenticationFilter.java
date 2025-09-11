5e65d7b2fd5bb5ea234442d3739bf18db8404ecf
Kang Min <min.kang@villanema.com>
2025-09-12 00:09:11 +0900

    JwtAuthenticationFilter Refactoring

--- security/src/main/java/edu/cnu/swacademy/security/common/JwtAuthenticationFilter.java
+++ security/src/main/java/edu/cnu/swacademy/security/common/JwtAuthenticationFilter.java
@@ -1,18 +1,16 @@
 package edu.cnu.swacademy.security.common;
 
-import java.io.IOException;
-
-import org.springframework.stereotype.Component;
-
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,12 +20,10 @@ public class JwtAuthenticationFilter implements Filter {
   private final JwtUtil jwtUtil;
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException {
     HttpServletRequest httpRequest = (HttpServletRequest) request;
     HttpServletResponse httpResponse = (HttpServletResponse) response;
-
+    
     try {
       String authHeader = httpRequest.getHeader("Authorization");
       if (authHeader == null || !authHeader.startsWith("Bearer ")) {
